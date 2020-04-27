61b61dbe83d972efc6f071ba9d4610e1baecd34f
everest486 <iron0210@naver.com>
2020-04-27 09:41:06 +0900

    add

---
+++ BitReverseOperatorExample.java
@@ -0,0 +1,20 @@
+public class BitReverseOperatorExample{
+	public static void main(String[] args){
+		int num1 = 10;
+		
+		int num2 = -num1;
+		String returnValue = toBinaryString(num1);
+		System.out.println(returnValue);
+		
+		System.out.println(num2);
+		returnValue = toBinaryString(num2);
+		System.out.println(returnValue);
+	}
+	public static String toBinaryString(int value){
+		String str = Integer.toBinaryString(value);
+		while(str.length() < 32) {
+			str = "0"+str;
+		}
+		return str;
+	}
+}
