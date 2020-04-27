ea9434d098bb1fcb69daba142dbf9fa83cfc5789
everest486 <iron0210@naver.com>
2020-04-27 11:53:39 +0900

    add

--- CharOperationExample.java
+++ CharOperationExample.java
@@ -4,7 +4,7 @@ public class CharOperationExample {
         int v1 = 5;
         int v2 = 2;
 
-        int result = v1 / v2;
+          int result = v1 / v2;
         double result1 = (double) v1 /v2;
         System.out.println(result1);
 
@@ -13,17 +13,14 @@ public class CharOperationExample {
 
         System.out.println("두수의 차이는: " + minus(v1, v2));
         System.out.println("나눈 몫 : " + divide(v1, v2));
-    }
+          }
     public static double divide (int num1, int num2){
         return (double)num1/num2;    
     }
     public static int sum (int num1, int num2){
         int result = num1 + num2;
         return result;
-
-
     }
-
     public static int minus (int num1, int num2){
         int result = num1 - num2;
         return result;
